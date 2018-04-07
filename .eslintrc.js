21fac74e34f6ab6cf5639c7c4b5aff9e6a6f74ef
Marcelo Ucker <marceloq0@gmail.com>
2018-04-06 23:20:16 -0300

    change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "plugins": [
-        "import"
-    ]
+  "extends": [
+    "airbnb-base",
+    "plugin:jest/recommended"
+  ],
+  "plugins": [
+    "import",
+    "jest"
+  ],
+  "env": {
+    "jest/globals": true
+  },
+  "rules": {
+    "jest/no-identical-title": "error",
+    "jest/valid-expect": "error"
+  }
 };
